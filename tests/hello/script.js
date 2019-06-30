8e2bdbb37ee2257de2b2b44b63ee74fd0b3ea6f8
James-Livesey <jamesliveg@gmail.com>
2019-06-30 16:27:35 +0100

    Build docs and tests

--- tests/hello/script.js
+++ tests/hello/script.js
@@ -4,11 +4,13 @@
 core.unpack(ui.components);
 
 ui.screen = [
-    new Heading("Hello, world!"),
-    new Paragraph("This is a test of Adapt UI, the user interface design language by LiveG Technologies."),
-    new Button("View on GitHub", false, {}, {}, {
-        click: function() {
-            window.open("https://github.com/LiveG-Development/Adapt-UI");
-        }
-    })
+    new Container([
+        new Heading("Hello, world!"),
+        new Paragraph("This is a test of Adapt UI, the user interface design language by LiveG Technologies."),
+        new Button("View on GitHub", false, {}, {}, {
+            click: function() {
+                window.open("https://github.com/LiveG-Development/Adapt-UI");
+            }
+        })
+    ])
 ];
